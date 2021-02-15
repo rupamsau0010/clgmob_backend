6607926637f2b6ccd62ae73c9db40a3e0c18a5db
Rupam Sau <rupamsau0010@users.noreply.github.com>
2021-02-15 23:10:32 +0530

    depandencies imported, server is running

---
+++ server.js
@@ -0,0 +1,26 @@
+// Import depandencies
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+// Import local depandencies
+
+
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+// MongoDB Database connection
+
+
+
+// local routes
+
+
+
+// Running the server on PORT 5000
+const port = process.env.PORT
+
+app.listen(port || 5000, () => {
+    console.log(`Server is running on port 5000 or ${port}`);
+})
